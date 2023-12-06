9d04c5e98e1edc6acbe205c9ed0e56670c0158d4
Filip Hanout <21IA07_HANOUT@spsjc.cz>
2023-12-06 14:02:34 +0100

    Commit Numero Dos

--- doucovani/Program.cs
+++ doucovani/Program.cs
@@ -1,19 +1,21 @@
 ﻿
-    
-        /*PROMĚNNÁ
 
-        //Místo v paměti PC kam si můžeme ukládat nějaká data(hodnoty výpočty, mezi-výpočtů, slova, znaky, čísla, výrazy, ..)
+/*PROMĚNNÁ
 
-        int - Z
-        double - R
-        string - texty
-        bool - true/false *(Jakákoliv NENULOVÁ hodnota, je vždy TRUE {1, 3, -6}
-        char - jednotlivé znaky (UNICODE, ASCII, ..)
+//Místo v paměti PC kam si můžeme ukládat nějaká data(hodnoty výpočty, mezi-výpočtů, slova, znaky, čísla, výrazy, ..)
 
-        C# = staticky typovaný jazyk
-        */
+int - Z
+double - R
+string - texty
+bool - true/false *(Jakákoliv NENULOVÁ hodnota, je vždy TRUE {1, 3, -6}
+char - jednotlivé znaky (UNICODE, ASCII, ..)
 
-        int cislo = 15;
+C# = staticky typovaný jazyk
+*/
+
+
+
+//int cislo = 15;
 
 //<datový typ> <identifikátor>; | = <hodnota> |;
 
@@ -21,7 +23,7 @@
 // +, -, *, /, % (modulo)
 // +=, -=, *=, /=, %= | ==> složené přirazené
 
-cislo = cislo + 1; //16 ==> ekvivalence => cislo += 1;
+//cislo = cislo + 1; //16 ==> ekvivalence => cislo += 1;
 
 //unární operátory (jsou jen na jedné straně operandu)
 // x++, ++x; inkrementace (+1)
@@ -29,6 +31,7 @@ cislo = cislo + 1; //16 ==> ekvivalence => cislo += 1;
 
 //-----------------------------------------
 
+//Praktická ukázka
 
 //int x = 10;
 //Console.WriteLine(x++);
@@ -77,19 +80,83 @@ cislo = cislo + 1; //16 ==> ekvivalence => cislo += 1;
 
 //---------------------------------------------------
 
-Console.WriteLine("Zadej nějaké hezké číslo: ");
-int hezkeCislo = int.Parse(Console.ReadLine());
+//PRAKTICKÁ UKÁZKA
 
-if (hezkeCislo % 2 == 0)
+//Console.WriteLine("Zadej nějaké hezké číslo: ");
+//int hezkeCislo = int.Parse(Console.ReadLine());
+
+//if (hezkeCislo % 2 == 0)
+//{
+//   Console.ForegroundColor = ConsoleColor.Green;
+//   Console.WriteLine("HoVn&o");
+//   Console.ForegroundColor = ConsoleColor.White;
+//}
+//else
+//{
+//   Console.ForegroundColor = ConsoleColor.Red;
+//   Console.WriteLine("Kočička");
+//   Console.ForegroundColor = ConsoleColor.White;
+//}
+
+
+//RELAČNÍ OPERÁTORY
+//<, <=, >, >=, ==, !=
+
+// = -> přirazuje hodnotu
+// == -> testuje hodnotu
+
+
+//---------------------------------------
+
+//PRAKTICKÁ UKÁZKA
+
+//Console.WriteLine("Zadejte číslo A: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Zadejte číslo B: ");
+//int b = int.Parse(Console.ReadLine());
+
+
+
+//if (a > b)
+//{
+//    Console.WriteLine("A je větší jak B");
+//}
+//else if (a == b)
+//{
+//    Console.WriteLine("A je rovno B");
+//}
+//else
+//{
+//    Console.WriteLine("A je menší jak B");
+//}
+
+//---------------------------------------
+
+//---------------------------------------
+
+//DALŠÍ PRAKTICKÁ UKÁZKA
+
+while (true)
 {
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("HoVn&o");
-    Console.ForegroundColor = ConsoleColor.White;
+
+    Console.WriteLine("Zadej číslo v rozsahu od 10-20 nebo 50-60: ");
+    int x = int.Parse(Console.ReadLine());
+
+    // AND -> &&
+    // OR -> ||
+    // NOT -> !
+
+    if ((x >= 10) && (x <= 20) || ((x >= 50) && (x <= 60)))
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Trefil(a) ses! :)");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Jdi doprdele ty hromado promrdanýho masa!");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
 }
-else
-{
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Kočička");
-    Console.ForegroundColor = ConsoleColor.White;
-}
-    
+
